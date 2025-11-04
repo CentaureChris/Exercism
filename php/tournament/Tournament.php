b2d267505891bb6c234b4e6bbe46d0b7746cda7a
Chris Centaure <c.centaure972@gmail.com>
2025-11-05 00:02:24 +0100

    add some Robot name php and poker php exercices

---
+++ php/tournament/Tournament.php
@@ -0,0 +1,133 @@
+<?php
+
+/*
+ * By adding type hints and enabling strict type checking, code can become
+ * easier to read, self-documenting and reduce the number of potential bugs.
+ * By default, type declarations are non-strict, which means they will attempt
+ * to change the original type to match the type specified by the
+ * type-declaration.
+ *
+ * In other words, if you pass a string to a function requiring a float,
+ * it will attempt to convert the string value to a float.
+ *
+ * To enable strict mode, a single declare directive must be placed at the top
+ * of the file.
+ * This means that the strictness of typing is configured on a per-file basis.
+ * This directive not only affects the type declarations of parameters, but also
+ * a function's return type.
+ *
+ * For more info review the Concept on strict type checking in the PHP track
+ * <link>.
+ *
+ * To disable strict typing, comment out the directive below.
+ */
+
+declare(strict_types=1);
+
+class Tournament
+{
+
+    public string $competitionResume;
+
+    public function __construct()
+    {
+    }
+
+    public function tally($scoresResume): string
+    {
+        $teamHeaderWithSpaces = "Team                           ";
+        $header = $teamHeaderWithSpaces."| MP |  W |  D |  L |  P";
+        $scores = $header;
+        $scoresResume = $this->getStats($scoresResume);
+        foreach ($scoresResume as $team => $stats) {
+            $new = $team . substr($teamHeaderWithSpaces, strlen($team));
+            $new = substr($new, 0, strlen($teamHeaderWithSpaces)-1);   
+            $scores .= "\n" . $new . " |  " . ($stats["W"] + $stats["D"] + $stats["L"]) . " |  " . $stats["W"] . " |  " . $stats["D"] . " |  " . $stats["L"] . " |  " . $stats["P"];
+        }
+        return $scores;
+    }
+
+    public function formatFile($resume): array
+    {
+        $matches = explode("\n", trim($resume));
+        return $matches;
+    }
+
+    public function getResult(string $resultLine): array|int
+    {
+        $result = explode(";", $resultLine);
+        $outcome = $result[2];
+
+        switch ($outcome) {
+            case 'win':
+                return [3, 0];
+            case 'loss':
+                return [0, 3];
+            case 'draw':
+                return [1, 1];
+            default:
+                return 0;
+        }
+    }
+
+    public function getStats($resume)
+    {
+        $matches = $this->formatFile($resume);
+        $stats = [];
+
+        foreach ($matches as $match) {
+            $match = explode(";", $match);
+            $team1 = $match[0];
+            $team2 = $match[1];
+
+            if (!array_key_exists($team1, $stats)) {
+                $stats[$team1] = ["W" => 0, "D" => 0, "L" => 0, "P" => 0];
+            }
+            switch ($match[2]) {
+                case 'win':
+                    $stats[$team1]["W"] += 1;
+                    $stats[$team1]["P"] += 3;
+                    break;
+                case 'loss':
+                    $stats[$team1]["L"] += 1;
+                    break;
+                case 'draw':
+                    $stats[$team1]["D"] += 1;
+                    $stats[$team1]["P"] += 1;
+                    break;
+            }
+
+
+            if (!array_key_exists($team2, $stats)) {
+                $stats[$team2] = ["W" => 0, "D" => 0, "L" => 0, "P" => 0];
+            }
+            switch ($match[2]) {
+                case 'win':
+                    $stats[$team2]["L"] += 1;
+                    break;
+                case 'loss':
+                    $stats[$team2]["W"] += 1;
+                    $stats[$team2]["P"] += 3;
+                    break;
+                case 'draw':
+                    $stats[$team2]["D"] += 1;
+                    $stats[$team2]["P"] += 1;
+                    break;
+            }
+        }
+        return $stats;
+    }
+}
+
+$resume = "Allegoric Alaskans;Blithering Badgers;win
+Devastating Donkeys;Courageous Californians;draw
+Devastating Donkeys;Allegoric Alaskans;win
+Courageous Californians;Blithering Badgers;loss
+Blithering Badgers;Devastating Donkeys;loss
+Allegoric Alaskans;Courageous Californians;win";
+
+$tournament = new Tournament();
+
+print '<pre>';
+print $tournament->tally($resume);
+
--- typescript/dnd-character/dnd-character.ts
+++ typescript/dnd-character/dnd-character.ts
@@ -1,49 +1,42 @@
-export class Dice {
-  public static value: number;
-
-  constructor(){
+class Dice {  
+  constructor () {
   }
 
-  public static throwDice = ():number => {
-    return Math.floor(Math.random() * 6) +1;
+  public static roll (faces:number) :number {
+    if(faces < 1) throw new Error('A roll must have minimum 1 face!')
+    return Math.floor(Math.random() * faces) +1;
   }
 }
 
 export class DnDCharacter {
+  public strength:number;
+  public dexterity:number;
+  public constitution:number;
+  public intelligence:number;
+  public wisdom:number;
+  public charisma:number;
+  public hitpoints:number;
 
-  public strength: number;
-  public dexterity: number;
-  public constitution: number;
-  public intelligence: number;
-  public wisdom: number;
-  public charisma: number;
-  public hitpoints: number;
-
-  constructor(){
-     this.strength = DnDCharacter.generateAbilityScore();
-     this.dexterity = DnDCharacter.generateAbilityScore();
-     this.constitution = DnDCharacter.generateAbilityScore();
-     this.intelligence = DnDCharacter.generateAbilityScore();
-     this.wisdom = DnDCharacter.generateAbilityScore();
-     this.charisma = DnDCharacter.generateAbilityScore();
-     this.hitpoints = 10 + DnDCharacter.getModifierFor(this.constitution);
-
+  public constructor () {
+    this.strength = DnDCharacter.generateAbilityScore();
+    this.dexterity = DnDCharacter.generateAbilityScore();
+    this.constitution = DnDCharacter.generateAbilityScore();
+    this.intelligence = DnDCharacter.generateAbilityScore();
+    this.wisdom = DnDCharacter.generateAbilityScore();
+    this.charisma = DnDCharacter.generateAbilityScore();
+    this.hitpoints = 10 + DnDCharacter.getModifierFor(this.constitution);
   }
 
   public static generateAbilityScore(): number {
-    const throwsValue = [Dice.throwDice(),Dice.throwDice(),Dice.throwDice(),Dice.throwDice()]
-    .sort((a, b) => b - a)
-    .slice(0, 3) 
-    .reduce((acc, element) => acc + element,0);
-
-    return throwsValue;
+     const throws = [Dice.roll(6),Dice.roll(6),Dice.roll(6),Dice.roll(6)]
+      .sort((a:number, b:number) => a - b);
+     throws.shift();
+     return throws.reduce((a:number, b:number) => a + b, 0);
   }
 
   public static getModifierFor(abilityValue: number): number {
-    const value = (abilityValue - 10)/2
-    return Math.floor(value)
+    return Math.floor((abilityValue - 10) / 2);
   }
 }
-
 
 
